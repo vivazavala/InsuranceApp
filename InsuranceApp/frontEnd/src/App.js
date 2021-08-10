1e2842a6cdc70455b12daf5cb64bf9683af03d4d
vivazavala1 <kennethz180@yahoo.com>
2021-08-10 16:33:37 -0500

    Form Component edited

--- InsuranceApp/frontEnd/src/App.js
+++ InsuranceApp/frontEnd/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom"
-import NavBar from "./Components/NavBar";
-import Axios from 'axios';
+import NavBar from "./Components/Form";
+
 function App() {
     return (
       <Router>
             <div className="App">
-                <NavBar/>
+                <Form/>
             Hello world
      </div>
             </Router>
---
+++ InsuranceApp/frontEnd/src/Components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+
+function Form() {
+    return (
+       
+            <div className="Form">
+               
+            Hello world
+     </div>
+   
+    );
+}
+
+export default Form;
